7748f1e1ad776ca5d9ddff7e9d0019166a7649bc
ishtiyak98 <rifatishtiyak@gmail.com>
2022-04-03 20:51:25 +0600

    initial upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hascasc </h1>
+       
+    </div>
+  );
+}
+
+export default App;
